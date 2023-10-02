56d6194a326c229b00e0c3d11a800fb07d969388
Momen Qudaih <momenqudaih1@gmail.com>
2023-10-02 10:59:33 +0300

    initial file structure setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <div>
+            <p className="text-3xl font-medium text-sky-700">
+                Hello Next 13.5s
+            </p>
+            <Button variant="destructive">click me</Button>
+        </div>
+    );
+}
